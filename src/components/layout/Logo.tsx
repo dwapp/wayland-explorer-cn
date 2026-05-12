a0fd8d860aa6b1cc3d7744ced78fc6a041ca9ed8
rewine <luhongxu@deepin.org>
2026-05-12 19:17:52 +0800

    Fix logo path for new GitHub Pages base URL

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'wouter'
 export const Logo: React.FC = () => (
     <Link href="/">
         <a href="/" className="flex items-center">
-            <img className="h-8 w-auto" src="/protocols/logo.svg" alt="Logo" />
+            <img className="h-8 w-auto" src="/wayland-explorer-cn/logo.svg" alt="Logo" />
             <span
                 className="font-semibold ml-2 truncate"
                 title="Wayland Explorer"
